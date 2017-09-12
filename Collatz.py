85c700120e3effa8384083a3c347dcecfca3d30c
yyuto6012 <(yyabu0618@gmail.com)>
2017-09-12 15:03:32 -0500

    collatz_eval

--- Collatz.py
+++ Collatz.py
@@ -10,13 +10,14 @@
 # collatz_read
 # ------------
 
-
 def collatz_read(s):
     """
     read two ints
     s a string
     return a list of two ints, representing the beginning and end of a range, [i, j]
     """
+
+    # what's s
     a = s.split()
     return [int(a[0]), int(a[1])]
 
@@ -27,12 +28,23 @@ def collatz_read(s):
 
 def collatz_eval(i, j):
     """
+    collatz circle
     i the beginning of the range, inclusive
     j the end       of the range, inclusive
     return the max cycle length of the range [i, j]
     """
-    # <your code>
-    return 1
+    circle = 1
+    c_list = []
+    for c in list(range(i, j+1)):
+        if c > 0:
+            while c != 1:
+                if c%2 == 0:
+                    c = c / 2
+                else:
+                    c = c * 3 + 1
+                circle += 1
+            c_list.append(circle)
+    return sorted(c_list, reverse=True)[0]
 
 # -------------
 # collatz_print
